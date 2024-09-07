926b1de875155cb97ad1262cb1390b4e98ee5102
Kym Carabeo <yuudachipoikancolle@gmail.com>
2024-09-07 13:08:31 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import {formatCurrency} from "../utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D',));
+import {renderPaymentSummary} from "./paymentSummary.js";
 
 export function renderOrderSummary(){
     let cartSummaryHTML = '';
@@ -137,6 +132,7 @@ export function renderOrderSummary(){
 
             container.remove();
             calculateCartQuantity();
+            renderPaymentSummary();
         })
     })
 
@@ -179,6 +175,7 @@ export function renderOrderSummary(){
         }
 
         updateQuantity(saveId, inputQuantity);
+        renderPaymentSummary();
         
         document.querySelector(`.quantity-label-${saveId}`).innerHTML = inputQuantity;
     }
@@ -205,6 +202,7 @@ export function renderOrderSummary(){
             const {deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 
