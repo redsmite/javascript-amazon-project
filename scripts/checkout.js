d9d691dd063510392ca0f5ec6d5688264fbef84c
Kym Carabeo <yuudachipoikancolle@gmail.com>
2024-12-03 11:18:24 +0800

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
-import '../data/car.js';
+//import '../data/car.js';
+
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
